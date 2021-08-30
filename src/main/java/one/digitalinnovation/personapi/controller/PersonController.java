3ace0f36c148c4bb13ccff78a970ed6cb6f3ed9e
RBANDEIRA72 <rogermendesbandeira@gmail.com>
2021-08-30 19:59:08 -0300

    Atualizacao - MAPPER

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,28 +1,31 @@
 package one.digitalinnovation.personapi.controller;
 
+import lombok.AllArgsConstructor;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
-import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
-    private PersonService personService;
+    private final PersonService personService;
 
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
+//    @Autowired
+//    public PersonController(PersonService personService) {
+//        this.personService = personService;
+//    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody Person person){
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
 
-        return personService.createPerson(person);
+        return personService.createPerson(personDTO);
 
     }
 
